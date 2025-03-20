19b1f4f629c7f864453526855452249cc7d359ba
Abhishek Singh <imabhiehekk20@gmail.com>
2025-03-20 15:50:06 +0530

    Overflow issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react"
 import './App.css';
 import Header from './Component/Header/Header';
 import Home from './Component/Homes/Home';
